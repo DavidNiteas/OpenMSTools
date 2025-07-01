e01f3154a4238e6d390ef1a8d02ddb2c34caba27
daiqinliang-305b2 <david.ql.d@foxmail.com>
2025-07-01 17:01:57 +0800

    feat: 添加FeatureMap类以处理特征映射数据
    新增了一个FeatureMap类，用于处理特征映射数据。该类继承自BaseModel，并包含两个主要的数据结构：feature_info和hulls。
    
    - feature_info是一个包含特征信息的DataFrame。
    - hulls是一个包含特征凸包信息的DataFrame。
    
    FeatureMap类提供了以下几个静态方法：
    - get_feature_metadata: 从单个特征中提取元数据信息。
    - get_feature_info: 从特征映射中提取特征信息，并合并特征的元数据信息到同一个DataFrame。
    - get_hull_range: 计算单个凸包的起始和结束m/z与RT值。
    - get_hulls: 从特征映射和特征XIC中提取所有凸包的信息。
    
    此外，FeatureMap类还提供了一个类方法from_oms，用于从OpenMS的FeatureMap和MSChromatogram对象创建FeatureMap实例，并将实验名称添加到索引中以区分不同的实验数据。

---
+++ OpenMSTools/ms_tools/data_wrapper_structs/features.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+from typing import Literal
+
+import dask
+import dask.bag as db
+import numpy as np
+import pandas as pd
+import pyopenms as oms
+from pydantic import BaseModel, ConfigDict
+
+
+class FeatureMap(BaseModel):
+
+    model_config = ConfigDict({"arbitrary_types_allowed": True})
+
+    feature_info: pd.DataFrame
+    hulls: pd.DataFrame
+
+    @staticmethod
+    def get_feature_metadata(feature: oms.Feature) -> dict[
+        Literal[
+            'hull_num',"hull_mz","hull_rt","hull_intensity",
+            "isotope_pattern",
+            "adduct_type","adduct_mass",
+        ],
+        str | float | int | np.ndarray
+    ]:
+        all_keys = []
+        feature.getKeys(all_keys)
+        all_keys = set(all_keys)
+        metadata = {
+            "hull_num": feature.getMetaValue("num_of_masstraces"),
+            "hull_mz": np.array(feature.getMetaValue("masstrace_centroid_mz")),
+            "hull_rt": np.array(feature.getMetaValue("masstrace_centroid_rt")),
+            "hull_intensity": np.array(feature.getMetaValue("masstrace_intensity")),
+            "isotope_pattern": np.cumsum(feature.getMetaValue("isotope_distances")),
+        }
+        if "dc_charge_adducts" in all_keys:
+            metadata["adduct_type"] = feature.getMetaValue("dc_charge_adducts")
+            metadata["adduct_mass"] = feature.getMetaValue("dc_charge_adduct_mass")
+        return metadata
+
+    @staticmethod
+    def get_feature_info(feature_map: oms.FeatureMap) -> pd.DataFrame:
+        feature_info = feature_map.get_df()[
+            ["RT","mz","intensity","MZstart","RTstart","MZend","RTend"]
+        ]
+        feature_bag = db.from_sequence(feature_map)
+        feature_metadata_bag = feature_bag.map(FeatureMap.get_feature_metadata)
+        feature_metadata_list = dask.compute(
+            feature_metadata_bag, scheduler="threads"
+        )[0]
+        feature_metadata_df = pd.DataFrame(
+            feature_metadata_list,
+            index=feature_info.index
+        )
+        feature_info = pd.concat([feature_info, feature_metadata_df], axis=1)
+        return feature_info
+
+    @staticmethod
+    def get_hull_range(
+        hull: oms.ConvexHull2D,
+    ) -> dict[
+        Literal["MZstart","RTstart","MZend","RTend"],
+        float
+    ]:
+        rt_points = hull.getHullPoints()[:,0]
+        mz_points = hull.getHullPoints()[:,1]
+        return {
+            "MZstart": np.min(mz_points),
+            "RTstart": np.min(rt_points),
+            "MZend": np.max(mz_points),
+            "RTend": np.max(rt_points),
+        }
+
+    @staticmethod
+    def get_hulls(
+        feature_map: oms.FeatureMap,
+        feature_xic: dict[dict[oms.MSChromatogram]],
+    ) -> pd.DataFrame:
+        rt_hulls = {}
+        for feature_rt_hulls in feature_xic:
+            for rt_hull in feature_rt_hulls:
+                rt_hulls[rt_hull.getNativeID().replace("_","::")] = rt_hull
+        mz_hulls = {}
+        for feature in feature_map:
+            for i,mz_hull in enumerate(feature.getConvexHulls()):
+                mz_hulls[f"{feature.getUniqueId()}::{i}"] = mz_hull
+        hulls = []
+        hulls_id = list(mz_hulls.keys())
+        for hull_id in hulls_id:
+            hull = {}
+            hull.update(FeatureMap.get_hull_range(mz_hulls[hull_id]))
+            rt_points, intens_points = rt_hulls[hull_id].get_peaks()
+            mz_points = mz_hulls[hull_id].getHullPoints()[:,1][:len(rt_points)]
+            hull['rt_points'] = rt_points
+            hull['mz_points'] = mz_points
+            hull['intens_points'] = intens_points
+            hulls.append(hull)
+        hulls = pd.DataFrame(hulls, index=hulls_id)
+        return hulls
+
+    @classmethod
+    def from_oms(
+        cls,
+        feature_map: oms.FeatureMap,
+        feature_xic: list[list[oms.MSChromatogram]],
+        exp_name: str,
+    ) -> FeatureMap:
+        feature_info = cls.get_feature_info(feature_map)
+        hulls = cls.get_hulls(feature_map, feature_xic)
+        feature_info.index = feature_info.index.map(lambda x: f"{exp_name}::{x}")
+        feature_info.index.name = "feature_id"
+        hulls.index = hulls.index.map(lambda x: f"{exp_name}::{x}")
+        hulls.index.name = "hull_id"
+        return cls(feature_info=feature_info, hulls=hulls)
