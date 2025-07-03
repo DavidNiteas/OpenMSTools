5c4032b442e3c25404cb198c98836cac917bd855
daiqinliang-305b2 <david.ql.d@foxmail.com>
2025-07-03 14:33:29 +0800

    feat(ms_tools): 添加新的数据结构和函数
    在 `__init__.py` 文件中，引入了新的数据结构 `ConsensusMap`, `FeatureMap`, `SpectrumMap`, `XICMap` 以及函数 `link_ms2_and_feature_map`。这些新的数据结构和函数将有助于更高效地处理和操作质谱数据。
    
    - 添加 `ConsensusMap` 结构用于表示共识图谱
    - 添加 `FeatureMap` 结构用于表示特征图谱
    - 添加 `SpectrumMap` 结构用于表示光谱图谱
    - 添加 `XICMap` 结构用于表示XIC图谱
    - 添加 `link_ms2_and_feature_map` 函数用于链接MS2图谱和特征图谱
    
    这些更改扩展了 `ms_tools` 模块的功能，使得开发者可以更方便地进行质谱数据分析。

--- OpenMSTools/ms_tools/__init__.py
+++ OpenMSTools/ms_tools/__init__.py
@@ -3,6 +3,13 @@ from .align_tools import RTAligner, RTAlignerConfig
 from .centroiding_tools import Centrizer, CentrizerConfig
 from .consensus_tools import FeatureLinker, FeatureLinkerConfig
 from .data_wrapper import OpenMSDataWrapper
+from .data_wrapper_structs import (
+    ConsensusMap,
+    FeatureMap,
+    SpectrumMap,
+    XICMap,
+    link_ms2_and_feature_map,
+)
 from .features_tools import FeatureFinder, FeatureFinderConfig
 from .normalization_tools import SpectrumNormalizer, SpectrumNormalizerConfig
 from .smoothing_tools import TICSmoother, TICSmootherConfig
@@ -22,5 +29,10 @@ __all__ = [
     "SpectrumNormalizer",
     "SpectrumNormalizerConfig",
     "TICSmoother",
-    "TICSmootherConfig"
+    "TICSmootherConfig",
+    "SpectrumMap",
+    "FeatureMap",
+    "XICMap",
+    "ConsensusMap",
+    "link_ms2_and_feature_map",
 ]
